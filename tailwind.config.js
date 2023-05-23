01f67ce88d7b6dc0b3d4e64e1a7a63663a1c7d65
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-05-23 10:39:22 +0200

    wizard animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+import { fontFamily as _fontFamily } from 'tailwindcss/defaultTheme';
+import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
+
+export const content = [
+  './src/**/*.{js,ts,jsx,tsx}',
+  './components/**/*.{js,ts,jsx,tsx}'
+];
+export const theme = {
+  extend: {
+    colors: {
+      // brand: "#242526",
+      // brand: "#282A2F",
+      brand: '#222322'
+    },
+    fontFamily: {
+      sans: ['Inter var', ..._fontFamily.sans]
+    }
+  }
 };
+// eslint-disable-next-line no-undef
+export const plugins = [require('@tailwindcss/forms'), addVariablesForColors];
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme('colors'));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ':root': newVars
+  });
+}
